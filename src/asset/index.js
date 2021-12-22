e6131f2b87760ef495bf0833c74b025dd2f2f1a3
izubaire <m.zobaire@gmail.com>
2021-12-22 16:22:07 +0500

    adding folder structure

---
+++ src/asset/index.js
@@ -0,0 +1,26 @@
+export { default as visionService } from "./visionService.svg";
+export { default as aboutUs } from "./aboutUs.svg";
+
+// export { default as avatar } from "./avatar.svg";
+// export { default as avatorImage } from "./avatar.svg";
+
+export { default as contactUs } from "./contactUs.svg";
+export { default as crpExpert } from "./crpExpert.svg";
+export { default as downloadForm } from "./downloadForm.svg";
+export { default as home1 } from "./home1.svg";
+export { default as home2 } from "./home2.svg";
+export { default as home2Bottom } from "./home2Bottom.svg";
+export { default as instantForms } from "./instantForms.svg";
+export { default as jobs } from "./jobs.svg";
+export { default as lacyFinancial } from "./lacyFinancial.svg";
+export { default as missionStatement } from "./missionStatement.svg";
+export { default as navicon } from "./navicon.svg";
+export { default as nursing } from "./nursing.svg";
+export { default as opportunity1 } from "./opportunity1.svg";
+export { default as opportunity2 } from "./opportunity2.svg";
+export { default as opportunity3 } from "./opportunity3.svg";
+export { default as opportunity4 } from "./opportunity4.svg";
+export { default as opportunity5 } from "./opportunity5.svg";
+export { default as opportunity6 } from "./opportunity6.svg";
+export { default as pharmacy } from "./pharmacy.svg";
+export { default as rehabTherapy } from "./rehabTherapy.svg";
